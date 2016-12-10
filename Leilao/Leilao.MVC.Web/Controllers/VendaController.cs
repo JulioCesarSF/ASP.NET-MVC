651bbd41b05167c6b335ba32eeb76b3014634e2f
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-10 19:15:20 -0200

    Fix Rota added VendaController e Vender View

---
+++ Leilao/Leilao.MVC.Web/Controllers/VendaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Leilao.MVC.Web.Controllers
+{
+    public class VendaController : Controller
+    {
+        // GET: Venda
+        public ActionResult Vender()
+        {
+            return View();
+        }
+    }
+}
