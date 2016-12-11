ea4eab98c15e8a68edc60fe2f7bfc8f4f46a7d35
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-11 09:41:33 -0200

    Arquivo sql para testes no banco

--- Leilao/Leilao.Dominio/Models/Negociacao.cs
+++ Leilao/Leilao.Dominio/Models/Negociacao.cs
@@ -27,6 +27,7 @@ namespace Leilao.Dominio.Models
         public string Status { get; set; }
         public int Tipo { get; set; }
         public int IdProduto { get; set; }
+        public Nullable<System.DateTime> Data { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Historico> Historico { get; set; }
