f06a072f4f060c8e2bae8c0bf2b95a87fc604f85
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-12 13:38:54 -0200

    Added api ProdutoApiController

---
+++ Bidme/Bidme.Service/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using Bidme.Dominio.Models;
+using Bidme.Persistencia.UnitsOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Mvc;
+
+namespace Bidme.Service.Controllers
+{
+    //limitar o acesso no webservice
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ProdutoController : ApiController
+    {
+        #region FIELDs
+        private UnitOfWork _unit = new UnitOfWork();
+        #endregion
+
+        //GET api/produto
+        //listar os produtos cadastrados
+        public ICollection<Produto> Get()
+        {
+            return _unit.ProdutoRepository.Listar();
+        }
+    }
+}
