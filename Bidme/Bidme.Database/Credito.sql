22996cb74a5dae53d9adbd6e611348e82dfed3e7
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-12 16:37:16 -0200

    Tabelas relacionadas a credito [OK]

--- Bidme/Bidme.Database/Credito.sql
+++ Bidme/Bidme.Database/Credito.sql
@@ -1,4 +1,9 @@
 ﻿CREATE TABLE [dbo].[Credito]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Total] INT NOT NULL, 
+    [IdTransacao] INT NOT NULL, 
+    [IdPessoa] INT NOT NULL, 
+    CONSTRAINT [FK_Credito_TransacaoCredito] FOREIGN KEY ([IdTransacao]) REFERENCES [TransacaoCredito]([Id]), 
+    CONSTRAINT [FK_Credito_Pessoa] FOREIGN KEY ([IdPessoa]) REFERENCES [Pessoa]([Id])
 )
