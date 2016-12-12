986880cc65073f09b81204084ee256ba1fd12c71
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-12 16:59:25 -0200

    Added CreditoViewModel e CreditoController

---
+++ Bidme/Bidme.MVC.Web/Controllers/CreditoController.cs
@@ -0,0 +1,33 @@
+﻿using Bidme.MVC.Web.ViewModels;
+using Bidme.Persistencia.UnitsOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bidme.MVC.Web.Controllers
+{
+    public class CreditoController : Controller
+    {
+        #region FIELDs
+        private UnitOfWork _unit = new UnitOfWork();
+        #endregion
+
+        #region GETs
+        [HttpGet]
+        public ActionResult ComprarCredito()
+        {
+            return View();
+        }
+        #endregion
+
+        #region POSTs
+        [HttpPost]
+        public ActionResult ComprarCredito(CreditoViewModel model)
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ Bidme/Bidme.MVC.Web/ViewModels/CreditoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Bidme.Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bidme.MVC.Web.ViewModels
+{
+    public class CreditoViewModel
+    {
+
+        #region LISTs
+        public ICollection<Credito> Creditos { get; set; }
+        #endregion
+
+        #region Table Credito
+        public int IdCredito { get; set; }
+        public int Total { get; set; }
+        public int IdPessoa { get; set; }
+        #endregion
+
+        #region Table Transacao
+        public int IdTransacao { get; set; }
+        public System.DateTime Data { get; set; }
+        public decimal Valor { get; set; }
+        #endregion
+
+    }
+}
