f4089e9fcbcef18524d85c1f64cd951b5ac36494
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-12 12:54:55 -0200

    Added ErroControler, Default e 404 views

---
+++ Bidme/Bidme.MVC.Web/Controllers/ErroController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bidme.MVC.Web.Controllers
+{
+    public class ErroController : Controller
+    {
+        #region GETS
+        [HttpGet]
+        public ActionResult Default()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Erro404()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
